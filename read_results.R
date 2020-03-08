7398b8b78e0bbbd344a38b77dd03cff4c3230e60
tung2921 <35050694+tung2921@users.noreply.github.com>
2020-03-07 18:05:14 -0600

    modify seniorthesis.md to sample from non-nan values and add read_results.R for reading results in parallel

---
+++ read_results.R
@@ -0,0 +1,9 @@
+d=as.data.frame(rep(list(numeric(0)),2))
+colnames(d)=c("task",'p_value')
+rownames(d)
+for(i in 1:10){
+  file=paste("Output/task-",i,".RData",sep="")
+  attach(file)
+  d[i,]=unlist(results)
+  detach(pos=2)
+}
