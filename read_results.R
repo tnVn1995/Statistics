048ba0a71997dbcc1a00bf73fec9661c0de2bbdd
tung2921 <35050694+tung2921@users.noreply.github.com>
2020-03-17 16:51:02 -0500

    Create index.html: Try to push to the web

--- read_results.R
+++ read_results.R
@@ -1,7 +1,7 @@
 d=as.data.frame(rep(list(numeric(0)),2))
 colnames(d)=c("task",'p_value')
 rownames(d)
-for(i in 1:10){
+for(i in 1:500){
   file=paste("Output/task-",i,".RData",sep="")
   attach(file)
   d[i,]=unlist(results)
